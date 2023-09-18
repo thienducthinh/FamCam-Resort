d6b2f5d6ccf4f4347316989aea8252c1eff8b6b8
thienducthinh <96965880+thienducthinh@users.noreply.github.com>
2023-09-18 08:36:29 -0700

    Update HW8.sql

--- HW8.sql
+++ HW8.sql
@@ -6,3 +6,9 @@ AND GuideID NOT IN ('RH01', 'MR01');
 
 
 /* Query 2 */
+SELECT MAX(DATEDIFF(year, Hiredate, CAST (GETDATE() AS Date) )) AS 'Most Years',
+MIN(DATEDIFF(year, Hiredate, CAST(GETDATE () AS Date))) AS 'Least Years',
+AVG(DATEDIFF(year, Hiredate, CAST (GETDATE () AS Date))) AS 'Average Years'
+FROM EMPLOYEE;
+
+
