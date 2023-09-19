714b087827c1dd4acb4d83822ef46c48edda18bf
thienducthinh <96965880+thienducthinh@users.noreply.github.com>
2023-09-18 22:57:50 -0700

    Update HW8.sql

--- HW8.sql
+++ HW8.sql
@@ -1,3 +1,74 @@
+/* Query 1: Write a simple select * query to review the Condo table first. You should see 45 records. */
+
+SELECT * FROM CONDO;
+
+
+/* Query 2: Based on the same table definition of Condo, create a duplicate table, New_Condo_A, by using the Select * into statement to populate the new table with all Building A records. Complete this question with only one SQL statement. */
+
+SELECT * INTO NEW_CONDO_A
+FROM CONDO
+WHERE BldgNum = 'A';
+
+SELECT * FROM NEW_CONDO_A;
+
+
+/* Query 3: Make 2 changes to 3-bedroom condos (there are 3 of them) in New_Condo_A, 1) add 15% to the daily rate and 2) add 200 square feet. Display the updated result for these 3 records. If your query result is incorrect, you might want to drop the new_condo_A table and recreate it before you retry. */
+
+UPDATE NEW_CONDO_A
+SET DailyRate = DailyRate * 1.15, SqrFt = SqrFt + 200
+WHERE Bdrms = 3;
+
+SELECT * FROM NEW_CONDO_A
+WHERE Bdrms = 3;
+
+
+/* Query 4: Delete 2-bedroom condos in New_Condo_A having the daily rate less than $145. Show your delete query statement and display all the remaining records in New_Condo_A. */
+
+DELETE FROM NEW_CONDO_A
+WHERE Bdrms = 2 AND DailyRate < 145;
+
+SELECT * NEW_CONDO_A;
+
+
+/* Basic SQL DQL Questions */
+/* Query 5: FamCation requires all guides to renew their certificate every two years from the previous certified date. Write a query to find guides who didn't renew their certificate on time. */
+
+SELECT GuideID AS 'Guide ID', CertDate AS 'Certification Date', CertRenewDate AS 'Certification Renewal Date'
+FROM GUIDE
+WHERE DATEADD(year, 2, CertDate) < CertRenewDate;
+
+
+/* Query 6: Select the 3 most senior employees in FamCation based on their HireDate. Display the employee ID, Employee Name (first name and last name), and hire date. Relabel the attributes according to the provided query result. */
+
+SELECT TOP(3) EmpID AS 'Employee ID', CONCAT(FName, ' ', LName) AS 'Employee Name', Hiredate AS 'Hire Date'
+FROM EMPLOYEE
+ORDER BY Hiredate;
+
+
+/* Query 7: Display a list of guest IDs who booked a condo in building A starting in the month of May 2021. Relabel the guestID as 'Guest ID'. */
+
+SELECT GuestID
+FROM BOOKING
+WHERE YEAR(StartDate) = 2021 AND MONTH(StartDate) = 5 AND BldgNum = 'A'
+
+
+/* Query 8: FamCation reservation report can only show 10 reservations on each page. The report prints the latest reservations first. Write a query to retrieve data for the second page. */
+
+SELECT *
+FROM RESERVATION
+ORDER BY RDate DESC
+OFFSET 10 ROWS
+FETCH NEXT 10 ROWS ONLY;
+
+
+/* Query 9: The current FamCation database stores employee's annual salary. Write a query to show 'Marketing' employee ID and their monthly salary. */
+
+SELECT EmpID AS 'Employee ID', Department, (Salary / 12) AS 'Monthly Salary',
+CAST((Salary / 12) AS DECIMAL(7, 2)) AS 'Monthly Salary in Decimal',
+CAST((Salary / 12) AS DECIMAL(5, 0)) AS 'Monthly Salary in Integer'
+FROM EMPLOYEE
+WHERE Department = 'Marketing'
+
 /* Query 1: Write a query to show all the reservations in 2020 for horseback riding activities (Act ID starting with ‘HB’) that were not led by guide RH01 or MR01. */
 SELECT * FROM RESERVATION
 WHERE YEAR(RDate) = 2020
