16fb4fb9d704e5c9e33b041eb1100dbb97746178
thienducthinh <96965880+thienducthinh@users.noreply.github.com>
2023-09-28 19:43:52 -0700

    Update HW11.sql

---
+++ HW11.sql
@@ -0,0 +1,26 @@
+SELECT DATEDIFF(DAY ,b.StartDate, b.EndDate) * c.DailyRate AS 'Total Condo Fee'
+FROM BOOKING b, CONDO c
+WHERE b.BldgNum = c.BldgNum
+AND b.UnitNum = c.UnitNum
+AND b.GuestID = 'G5'
+AND b.StartDate = '2021-08-03';
+
+
+CREATE PROCEDURE Create_Invoice (@guestid VARCHAR(4), @startdate DATE) 
+AS 
+BEGIN TRY 
+	BEGIN TRANSACTION
+		INSERT INTO INVOICE (BookID, GuestID, StartDate, CondoFee, ActivityFee) VALUES (
+		(SELECT BookID FROM BOOKING WHERE StartDate = @startdate AND GuestID = @guestid),
+		@guestid,
+		@startdate,
+		(SELECT DATEDIFF(DAY ,b.StartDate, b.EndDate) * c.DailyRate FROM BOOKING b, CONDO c 
+		WHERE b.BldgNum = c.BldgNum AND b.UnitNum = c.UnitNum AND b.GuestID = @guestid AND b.StartDate = @startdate),
+		(SELECT SUM(a.PPP * r.NumerInParty) FROM ACTIVITY a, RESERVATION r
+		WHERE a.ActID = r.ActID AND r.GuestID = @guestid AND r.RDate >= @startdate))
+	COMMIT
+END TRY 
+BEGIN CATCH 
+	Select ERROR_NUMBER() AS ErrorNumber , ERROR_MESSAGE() AS ErrorMessage;  
+	ROLLBACK 
+END CATCH
